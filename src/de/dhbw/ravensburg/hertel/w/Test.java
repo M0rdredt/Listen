0455f75263da0bd09d6c0986c21f44e18f6459fc
MBiegger <biegger.marius@gmail.com>
2019-04-28 19:30:20 +0200

    finalized toString method for LinkedLists. minor changes

--- src/de/dhbw/ravensburg/hertel/w/Test.java
+++ src/de/dhbw/ravensburg/hertel/w/Test.java
@@ -1,8 +1,14 @@
 package de.dhbw.ravensburg.hertel.w;
 
+import de.dhbw.ravensburg.hertel.w.LinkedList.LinkedList;
+
 public class Test {
 
     public static void main(String[] args) {
-	// write your code here
+        LinkedList list = new LinkedList();
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        System.out.println(list);
     }
 }
