6239681f8c913e0330feafe5994653f03105616d
M0rdredt <willi.hertel@web.de>
2019-06-07 14:42:31 +0200

    Start Impl Set an SetElement

--- src/de/dhbw/ravensburg/hertel/w/Set/Set.java
+++ src/de/dhbw/ravensburg/hertel/w/Set/Set.java
@@ -2,81 +2,60 @@ package de.dhbw.ravensburg.hertel.w.Set;
 
 import de.dhbw.ravensburg.hertel.w.Abstract.AbstractElement;
 import de.dhbw.ravensburg.hertel.w.Abstract.AbstractList;
+import de.dhbw.ravensburg.hertel.w.LinkedList.LinkedList;
+import de.dhbw.ravensburg.hertel.w.LinkedList.LinkedListElement;
 
 //TODO: alles
-public class Set extends AbstractList {
+public class Set<T> extends LinkedList {
+
     @Override
-    public AbstractElement getHead() {
+    public void add(Object value){
+        if(!super.contains(value))
+            super.add(value);
+    }
+    public void addAll(Set set){
+        Object[] objects = set.returnAsArray();
+        for (Object o :
+                objects) {
+            this.add(o);
+        }
+    }
+
+
+
+    public static Set getUnion(Set set1, Set set2){
+        Set toReturn = new Set();
+        toReturn.addAll(set1);
+        toReturn.addAll(set2);
+        return toReturn;
+    }
+
+    public Set getUnion(Set set2){
+        return getUnion(this, set2);
+    }
+
+
+    public Set getIntersection( Set set2){
+        return getIntersection(this, set2);
+    }
+
+    public Set getDifferenceQuantity(Set set2){
+        return getDifferenceQuantity(this, set2);
+    }
+
+    public Set getSymmetricDifference(Set set2){
+        return getSymmetricDifference(this, set2);
+    }
+
+    public static Set getIntersection(Set set1, Set set2){
         return null;
     }
 
-    @Override
-    public void swapElements(AbstractElement element1, AbstractElement element2) {
-
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public int size() {
-        return 0;
-    }
-
-    @Override
-    public void add(Object value) {
-
-    }
-
-    @Override
-    public void addAll(AbstractList list) {
-
-    }
-
-    @Override
-    public void removeAll() {
-
-    }
-
-    @Override
-    public boolean contains(Object value) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(AbstractList list) {
-        return false;
-    }
-
-    @Override
-    public void removeParticularObj(Object value) {
-
-    }
-
-    @Override
-    public Object[] returnAsArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public String toString() {
+    public static Set getDifferenceQuantity(Set set1, Set set2){
         return null;
     }
 
-    @Override
-    public void quickSort() {
-
-    }
-
-    @Override
-    public void otherSort() {
-
-    }
-
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+    public static Set getSymmetricDifference(Set set1, Set set2){
+        return null;
     }
 }
