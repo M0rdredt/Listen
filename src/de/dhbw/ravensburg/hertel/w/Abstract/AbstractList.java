406bffc65a702f01f21fe2c2bbb57b58968ffb39
M0rdredt <willi.hertel@web.de>
2019-07-09 18:11:09 +0200

    Cleaning-Up

--- src/de/dhbw/ravensburg/hertel/w/Abstract/AbstractList.java
+++ src/de/dhbw/ravensburg/hertel/w/Abstract/AbstractList.java
@@ -1,6 +1,6 @@
 package de.dhbw.ravensburg.hertel.w.Abstract;
 
-public abstract class AbstractList<T> implements Comparable, Sortable {
+public abstract class AbstractList<T> {
 
     AbstractElement head;
 
@@ -9,29 +9,13 @@ public abstract class AbstractList<T> implements Comparable, Sortable {
     public abstract boolean isEmpty();
     public abstract int size();
     public abstract void add(T value);
-    public void addAll(AbstractList list){
-        for (T value:(T[]) list.returnAsArray()
-        ) {
-            this.add(value);
-        }
-    }
+    public abstract void addAll(AbstractList list);
     public abstract void removeAll();
     public abstract boolean contains(T value);
-
-    public boolean containsAll(AbstractList list){
-        for (T value:(T[]) list.returnAsArray()
-        ) {
-            if(!this.contains(value))
-                return false;
-        }
-        return true;
-    }
-
+    public abstract boolean containsAll(AbstractList list);
     public abstract Object[] returnAsArray();
     public abstract String toString();
-
     public abstract AbstractElement getHead();
-
     public abstract void swapElements(AbstractElement element1, AbstractElement element2);
 
 
